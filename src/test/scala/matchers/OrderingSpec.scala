8d9026687d58181d8f8580c71e06f5b09784d889
kumo829 <amonmar2000@hotmail.com>
2022-05-01 08:50:42 +0900

    ➕ADD: performing expressive tests using matchers

---
+++ src/test/scala/matchers/OrderingSpec.scala
@@ -0,0 +1,36 @@
+package matchers
+
+import com.h2.services.Currency
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class OrderingSpec extends AnyFlatSpec with Matchers{
+  behavior of "Currency conversion cost in comparison"
+
+  it should "report equal cost for 10 USD, 10USD" in {
+    val tenUsd: Currency = "10 USD"
+    val anotherTenUsd: Currency = "10 USD"
+
+    tenUsd.costInDollars.amount should be >= anotherTenUsd.costInDollars.amount
+  }
+
+
+  it should "report high cost for 100 USD, 10USD" in {
+    val tenUsd: Currency = "10 USD"
+    val hundredUsd: Currency = "100 USD"
+
+    hundredUsd.costInDollars.amount should be > tenUsd.costInDollars.amount
+  }
+
+
+  it should "report less cost for 1 USD, 10USD" in {
+    val tenUsd: Currency = "10 USD"
+    val oneUsd: Currency = "1 USD"
+
+    oneUsd.costInDollars.amount should be < tenUsd.costInDollars.amount
+  }
+
+  it should "report CAD < USD" in {
+    "CAD" should be < "USD"
+  }
+}
